ca128834956716df8bce605ef68f3abc3c0e8b18
Tim N <tim@gurka.se>
2023-06-06 14:08:22 +0200

    chess.js + getPossibleMoves

--- src/lib/Chess.svelte
+++ src/lib/Chess.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
 	import { Chessground } from 'svelte-chessground';
+	import { Chess as ChessJS, SQUARES } from 'chess.js';
+
+	const chessJS = new ChessJS();
+
+	// Find all legal moves
+	function possibleMoves(chess: ChessJS) {
+		const dests = new Map();
+		SQUARES.forEach(s => {
+			const ms = chess.moves({square: s, verbose: true});
+			if (ms.length) dests.set(s, ms.map(m => m.to));
+		});
+		return dests;
+	}
+	export function getPossibleMoves() {
+		return possibleMoves(chessJS);
+	}
 </script>
 
 <Chessground/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import Chess from '$lib/Chess.svelte';
+
+	import { onMount } from 'svelte';
+
+	let chess:Chess;
+
+	onMount( () => {
+		console.log(chess.getPossibleMoves());
+	} );
 </script>
 
 <div style="max-width:512px;margin:0 auto;">
-	<Chess/>
+	<Chess bind:this={chess}/>
 </div>
