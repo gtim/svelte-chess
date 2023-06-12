44e57d4863ac4bae4886b5f54a8a27c1b74de921
Tim N <tim@gurka.se>
2023-06-13 00:53:44 +0200

    fix undefined-api edge case

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
 	import Chess from '$lib/Chess.svelte';
 	import type { Api } from '$lib/api.js'; // TODO should be imported via Chess.svelte?
 
-	let api: Api;
+	let api: Api | undefined;
 </script>
 
 <div style="max-width:512px;margin:0 auto;">
 	<Chess bind:api={api} />
-	<button on:click={()=>api.resetBoard()}>Reset board</button>
-	<button on:click={()=>api.undoLastMove()}>Undo</button>
-	<button on:click={()=>api.toggleOrientation()}>Flip board</button>
+	<button on:click={()=>api?.resetBoard()}>Reset board</button>
+	<button on:click={()=>api?.undoLastMove()}>Undo</button>
+	<button on:click={()=>api?.toggleOrientation()}>Flip board</button>
 </div>
 
 <style>
