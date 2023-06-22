627c9c2e4c748fee7a3f2781c3f71c4c6e523d62
Tim N <tim@gurka.se>
2023-06-22 14:53:54 +0200

    engine tests todo list

---
+++ test/engine.test.ts
@@ -0,0 +1,20 @@
+test.todo( "engine plays first move when white/both" );
+test.todo( "engine plays first move when white/both and game is reset" );
+test.todo( "engine plays first move when black/both and black-to-move position is loaded" );
+test.todo( "engine spends approx. moveTime ms on a move" );
+test.todo( "engine solves some example puzzles" );
+test.todo( "engine does not solve some example puzzle at low depth" );
+test.todo( "engine-vs-engine game plays out and ends properly (test from appropriate position)" );
+
+test.todo( "chessground interactions are disabled before engine is loaded" );
+test.todo( "chessground interactions are enabled after engine is loaded" );
+test.todo( "chessground interactions are disabled while engine is searching" );
+test.todo( "move() throws before engine is loaded" );
+test.todo( "move() throws while engine is searching" );
+
+test.todo( "egine moves or does not move correctly after undo()" );
+test.todo( "engine initialisation happens after uciok" );
+test.todo( "engine.ts checks that stockfish is ready before sending position+go" );
+test.todo( "opening moves are varied (opening book, stochastic test)" );
+test.todo( "on:uci forwards UCI messages");
+test.todo( "correct king is hilighted on check");
